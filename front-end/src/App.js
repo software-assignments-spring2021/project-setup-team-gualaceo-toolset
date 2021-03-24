a3d7a0b4201b8e0d501c7652afe45abd7766dbc6
Dennis Kuzminer <denniskuzminer@gmail.com>
2021-03-24 00:08:28 -0400

    Created add individual songs page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,15 +4,16 @@ import landing from "./pages/landing";
 import placeholder from "./pages/placeholder";
 import home from "./pages/home";
 import guest from "./pages/guest";
-import { Notifications } from 'react-push-notification';
+import { Notifications } from "react-push-notification";
 import groupmenu from "./pages/groupmenu";
 import groupMenuGuest from "./pages/groupmenu_guest";
 import generatedPlaylist from "./pages/generatedPlaylist";
 import addMyMusic from "./pages/addMyMusic";
-import members from "./pages/members.js";
-import membersGuest from "./pages/membersGuest.js";
-import bannedMembers from "./pages/bannedMembers.js";
+import members from "./pages/members";
+import membersGuest from "./pages/membersGuest";
+import bannedMembers from "./pages/bannedMembers";
 import generatedPlaylistGuest from "./pages/generatedPlaylistGuest";
+import addSongs from "./pages/addSongs";
 
 const App = () => {
   return (
@@ -25,20 +26,25 @@ const App = () => {
           <Route exact path="/home" component={home} />
           <Route exact path="/guest" component={guest} />
           <Route exact path="/groupmenu" component={groupmenu} />
-          <Route exact path="/groupMenuGuest" component= {groupMenuGuest}/>
+          <Route exact path="/groupMenuGuest" component={groupMenuGuest} />
           <Route
             exact
             path="/generatedPlaylist"
             component={generatedPlaylist}
           />
-          <Route exact path = "/generatedPlaylistGuest" component = {generatedPlaylistGuest} />
+          <Route
+            exact
+            path="/generatedPlaylistGuest"
+            component={generatedPlaylistGuest}
+          />
           <Route exact path="/addMyMusic" component={addMyMusic} />
           <Route exact path="/bannedMembers" component={bannedMembers} />
           <Route exact path="/members" component={members} />
           <Route exact path="/membersGuest" component={membersGuest} />
+          <Route exact path="/addSongs" component={addSongs} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 export default App;
