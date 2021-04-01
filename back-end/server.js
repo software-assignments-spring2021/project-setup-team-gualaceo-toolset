aa0026c5f28d1c8205c57d7aca34c699e3b58dba
ryan-bello <ryanbello48@gmail.com>
2021-03-31 20:48:49 -0400

    changed back-end port to 5000

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const server = require("./app")
 
-const port = 3001 //port to listen to for requests
+const port = 5000 //port to listen to for requests
 
 const listener = server.listen(port, () => {
   console.log(`Server running on port: ${port}`)
