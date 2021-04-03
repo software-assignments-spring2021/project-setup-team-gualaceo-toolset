59a3825ec4a3151f38439defbc440e78c16e50cb
ryan-bello <ryanbello48@gmail.com>
2021-04-03 15:18:25 -0400

    removed back-end from .gitignore, user_playlists endpoint is now functional

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,15 @@
 //import and instantiate express
+//import get_playlists from "./src/get_endpoints/user_playlists"
+const user_playlists = require("./src/get_endpoints/user_playlists")
+const user_id = require("./src/get_endpoints/user_id")
+
 const express = require("express")
 const app = express()
 
 app.use("/static", express.static("public"))
-app.get("/user_playlists/:bearer", )
-//Server logic will go here
 
-//export the epxress app to make it available to other modules
+app.use("/user_playlists/:bearer", user_id.get_user_id) // sets res.user_id to the user_id (if the bearer token is valid)
+app.get("/user_playlists/:bearer", user_playlists.get_playlists)
+
+//export the express app to make it available to other modules
 module.exports = app
--- back-end/src/get_endpoints/user_playlists.js
+++ back-end/src/get_endpoints/user_playlists.js
@@ -1,27 +1,55 @@
-import axios from "axios";
+const axios = require("axios")
+const set_authentication = require("../other/authentication.js").set_authentication
 
-const get_playlist = (req, res) => {
-  const bearer = req.params.bearer
+const get_playlists = async (req, res) => {
 
-  //need to first get the user ID
+    const bearer = req.params.bearer
+    const user_id = req.user_id //this is set by previous middleware
 
-  let offset = 0;
-  let limit = 50;
-  let total = 51; //total is unknown at start, must be retrieved from API
-  let allSongs = []
-  
-  while (offset + limit < total)
-  {
+    let offset = 0;
+    let limit = 50;
+    let total = 51; //total is unknown at start, must be retrieved from API
+    let user_playlists = []
     let data = {}
-    axios("https://api.spotify.com/v1/me/playlists")
-      .then((response) => {
-        data = response.data
-      })
-      .catch((err) => {
-        console.log("Something went wrong in the get_playlists endpoint")
-        console.error(err)
-        res.send('error') //Not sure about this error handling
+
+    if (!user_id)
+    {
+        console.log("Error: No user_id provided")
         return;
-      })
-  }
+    } 
+    if (!set_authentication(bearer, axios))
+    {
+        console.log("Error: could not run get_playlist due to bad authentication")
+        return;
+    }
+
+    do { //We must make several API calls to cycle through ALL of the user's playlists, as the Spotify API does not allow us to get all at once
+    await axios(`https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=${limit}`)
+        .then((response) => {
+            data = response.data
+
+            data.items.forEach(item => {
+                if (item.owner.id === user_id) //This is owned by the user making the request, so we append it
+                {
+                    user_playlists.push(item)
+                } 
+            }) //cycle through the playlist JSON objects, which contain additional information
+
+            total = data.total
+            offset += limit
+        })
+        .catch((err) => {
+            console.log("Error: Something went wrong in the get_playlists endpoint when calling the Spotify API")
+            console.error(err)
+            throw new Error("Something went wrong in the get_playlists endpoint when calling the Spotify API")
+            return;
+        })
+    } while (offset < total) //continue while still in range of the user's playlists
+
+    console.log("Successfully pulled user playlists from Spotify API")
+    return res.send(user_playlists)
+}
+
+module.exports = {
+    get_playlists: get_playlists
 }
