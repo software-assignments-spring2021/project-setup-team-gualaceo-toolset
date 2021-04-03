4fa5d6e02a8b972e6080d4b60f55dabdd62581d6
ryan-bello <ryanbello48@gmail.com>
2021-04-03 15:44:43 -0400

    added a few comments to the routing page

--- back-end/app.js
+++ back-end/app.js
@@ -6,11 +6,13 @@ const user_id = require("./src/get_endpoints/user_id")
 const express = require("express")
 const app = express()
 
-app.use("/static", express.static("public"))
+app.use("/static", express.static("public")) //Anything within the /public directory is delivered statically by accessing /static/filename in your browser
 
 app.use("/user_playlists/:bearer", user_id.get_user_id) // sets res.user_id to the user_id (if the bearer token is valid)
 app.get("/user_playlists/:bearer", user_playlists.get_playlists)
-app.use((error, req, res, next) => {
+
+//Handle any errors
+app.use((error, req, res, next) => { 
     res.status(error.staus || 500)
     return res.send(error.message)
 })
