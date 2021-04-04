5d209bdcb2090e176aaed30582f9133489ee9c4a
unknown <chriszheng2012@hotmail.com>
2021-04-04 05:06:55 -0400

    Pull recommended songs as a list of objects

--- back-end/src/get_endpoints/recommend_songs.js
+++ back-end/src/get_endpoints/recommend_songs.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 //const set_authentication = require("../other/authentication.js").set_authentication
 
-const recommend_songs = (req, res) => {   
+const recommend_songs = async (req, res) => {   
 
     /*if (!set_authentication(bearer, axios))
     {
@@ -9,27 +9,32 @@ const recommend_songs = (req, res) => {
         return;
     }*/
 
-    let token='Bearer '+ 'BQD0O441nMxaBbVSqlKs5rD-f3HRdUOAzr55aMRs3hw11Rp3Wiw9_LLdU625FXFozLMGpxA39OLD3mHeAoVkpXv6Z0m6TBAejxcBuLKxf_FAHxiYmkrr9uXzi8wv4utSWzBaHi7iLy8TBmP4rlmsWCnXE_VSBIaeVA';
-    let data={};
-    let limit=5;
-    let URL = `https://api.spotify.com/v1/recommendations?limit=${limit}&seed_genres=jazz`;
-    console.log(URL);
+    //put bearer token here
+    let token='Bearer '+ 'BQDg1sOJCX2i6s5StFImVWzfhnzockzg-o3ISL1yDjQLYbK_axDgbmlE5sb8tIhqkCDeCyC1ieSoevr3fiqYquLUsgRsO9Y70WA6q7PKNLsxHSb1LYXoA5UsItCFdP6WLg1V-932gPHHRDEMy42LUg454xUFYEXASA';
+    let tracks=[]
+    //target number of songs to be generated
+    let limit=2;
+    //market of to-be generated  song, optional
+    let market='US';
+    //comma separated list of spotify music id, used to generate songs(max 5)
+    let seed_tracks='7jIE9yaNiRlm3KqyFeSQiX,26OAl3xCnVQEv4z22BQ9bS';
+    let URL = `https://api.spotify.com/v1/recommendations?limit=${limit}&market=${market}&seed_tracks=${seed_tracks}`;
 
-
-
-
-    //let data
-    axios(URL, { 'headers': { 'Authorization': token } })
+    await axios(URL, { 'headers': { 'Authorization': token} })
         .then((response) => {
-            data = response.data
+            //recommended tracks, as an array of JSON objects
+            tracks=response.data.tracks
             console.log("Logging recommendation")
-            console.log(data)
+            console.log(tracks)
         })
         .catch((err) => {
             console.log("Something went wrong")
             console.error(err)
             return;
         })
+
+    console.log("Return recommended songs as a list of JSON objects")
+    return res.send(tracks)
 
 }
 
