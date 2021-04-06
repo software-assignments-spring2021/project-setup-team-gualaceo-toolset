9ee51cb9b53745457dda78c01a6c7c12a155bc8a
ryan-bello <ryanbello48@gmail.com>
2021-04-05 23:34:52 -0400

    small changes

--- front-end/src/components/logout.js
+++ front-end/src/components/logout.js
@@ -13,6 +13,7 @@ import { useHistory } from "react-router-dom";
 const styles = (theme) => ({});
 
 const handleLogout = (history) => {
+  //clear any Spotify session data
   localStorage.removeItem('auth_data')
   localStorage.removeItem('expiry_time')
   history.push("/")
