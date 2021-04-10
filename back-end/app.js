01388effde91d64830003a59d8b444ede4c01c91
ryan-bello <ryanbello48@gmail.com>
2021-04-10 12:49:17 -0400

    fixed imports

--- back-end/app.js
+++ back-end/app.js
@@ -5,8 +5,8 @@ const user_id = require("./requests/get/user_id");
 const user_playlists = require("./requests/get/user_playlists");
 const express = require("express");
 const app = express();
-const remove_tracks = require("./src/post_endpoints/remove_tracks")
-const follow_playlist = require("./src/put_endpoints/follow_playlist")
+const remove_tracks = require("./requests/post/remove_tracks.js")
+//const follow_playlist = require("./requests/put/follow_playlist")
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -51,9 +51,9 @@ app.use("/user_playlists/:bearer/:include_tracks", user_id.get_user_id) // sets 
 app.get("/user_playlists/:bearer/:include_tracks", user_playlists.get_playlists)
 app.get("/recommend_songs/:bearer/limit/:limit/seed_tracks/:seed_tracks", recommend_songs.recommend_songs)
 app.use("/remove_tracks/:bearer/:playlist_id/:track_id", user_id.get_user_id)
-app.use("/follow_playlist/:bearer/:playlist_id", user_id.get_user_id)
+//app.use("/follow_playlist/:bearer/:playlist_id", user_id.get_user_id)
 app.delete("/remove_tracks/:bearer/:playlist_id/:track_id", remove_tracks.remove_tracks)
-app.put("/follow_playlist/:bearer/:playlist_id", follow_playlist.follow_playlist)
+//app.put("/follow_playlist/:bearer/:playlist_id", follow_playlist.follow_playlist)
 
 //Handle any errors
 app.use((error, req, res, next) => {
