7c5d14bf9cd63466190e0645af0eeb92adb31c58
ma5081 <ma5081@nyu.edu>
2021-04-10 20:20:21 +0400

    added remove tracks function

--- back-end/app.js
+++ back-end/app.js
@@ -5,6 +5,8 @@ const user_id = require("./src/get_endpoints/user_id")
 const user_playlists = require("./src/get_endpoints/user_playlists")
 const express = require("express")
 const app = express()
+const remove_tracks = require("./src/post_endpoints/remove_tracks")
+const follow_playlist = require("./src/put_endpoints/follow_playlist")
 
 // This code disables CORS, it may be necessary for debugging.
 //***We should remove this in the final version***
@@ -19,6 +21,10 @@ app.use("/static", express.static("public")) //Anything within the /public direc
 app.use("/user_playlists/:bearer/:include_tracks", user_id.get_user_id) // sets res.user_id to the user_id (if the bearer token is valid)
 app.get("/user_playlists/:bearer/:include_tracks", user_playlists.get_playlists)
 app.get("/recommend_songs/:bearer/limit/:limit/seed_tracks/:seed_tracks", recommend_songs.recommend_songs)
+app.use("/remove_tracks/:bearer/:playlist_id/:track_id", user_id.get_user_id)
+app.use("/follow_playlist/:bearer/:playlist_id", user_id.get_user_id)
+app.delete("/remove_tracks/:bearer/:playlist_id/:track_id", remove_tracks.remove_tracks)
+app.put("/follow_playlist/:bearer/:playlist_id", follow_playlist.follow_playlist)
 
 //Handle any errors
 app.use((error, req, res, next) => { 
