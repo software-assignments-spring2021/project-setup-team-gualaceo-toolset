211df3eeb98eb6a576902c04173ba09d92f0f4ce
chriszheng2012 <chriszheng2012@hotmail.com>
2021-04-24 12:03:27 -0400

    Edited out test related codes

--- back-end/app.js
+++ back-end/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-/*
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   keepAlive: true,
@@ -32,9 +32,9 @@ mongoose.connect(uri, {
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB connection established");
-});*/
+});
 
-//Testing with a localhost database
+/*Testing with a localhost database
 const url = 'mongodb://127.0.0.1:27017/local-test'
 mongoose.connect(url, { useNewUrlParser: true,useUnifiedTopology: true })
 const connection = mongoose.connection
@@ -44,6 +44,7 @@ connection.once('open', _ => {
 connection.on('error', err => {
   console.error('connection error:', err)
 })
+*/
 
 
 const groupsRouter = require("./routes/groups");
