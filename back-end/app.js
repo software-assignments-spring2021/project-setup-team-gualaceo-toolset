ed1a1b9fe0b67aa15e8ebb8321b8623ed72ad9b2
chriszheng2012 <chriszheng2012@hotmail.com>
2021-05-03 02:11:43 -0400

    working on fixing post_playlist helper

--- back-end/app.js
+++ back-end/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-
+/*
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   keepAlive: true,
@@ -34,8 +34,9 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB connection established");
 });
+*/
 
-/*Testing with a localhost database
+//Testing with a localhost database
 const url = 'mongodb://127.0.0.1:27017/local-test'
 mongoose.connect(url, { useNewUrlParser: true,useUnifiedTopology: true })
 const connection = mongoose.connection
@@ -45,7 +46,7 @@ connection.once('open', _ => {
 connection.on('error', err => {
   console.error('connection error:', err)
 })
-*/
+
 
 
 const groupsRouter = require("./routes/groups");
